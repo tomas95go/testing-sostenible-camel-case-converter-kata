c8ecd443984133add915457923e80a96fcd24552
Tom <tomas95go@gmail.com>
2025-09-23 22:32:47 -0300

    gren: should return same string when given word has a capital first letter

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,15 @@ export function camelCaseConverter(word: string) {
     if(noWordIsGiven) {
         return '->';
     }
+    const splittedWord = word.split('');
+    const givenWordFirstLetter = splittedWord[0];
+    const givenWordFirstLetterLowerCase = splittedWord[0].toLowerCase();
+
+    const givenWordFirstLetterCharCode = givenWordFirstLetter.charCodeAt(0);
+    const givenWordFirstLetterCharCoderLowerCase = givenWordFirstLetterLowerCase.charCodeAt(0);
+
+    if(givenWordFirstLetterCharCode !== givenWordFirstLetterCharCoderLowerCase) {
+        return `${word} -> ${word}`;
+    }
+
 }
